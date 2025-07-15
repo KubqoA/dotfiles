acaddd135d5981849803712d28dce6ccf1a2b6b1
Jakub Arbet <hi@jakubarbet.me>
2025-07-15 07:04:23 +0200

    Switch to iwd

--- hosts/harmonium/system/networking.nix
+++ hosts/harmonium/system/networking.nix
@@ -1,27 +1,33 @@
-# TODO: use iwd, systemd-networkd, and some good ui/tui
 {
   config,
-  lib,
+  pkgs,
   ...
 }: {
   impermanence.directories = [
-    "/etc/NetworkManager/system-connections"
-    "/var/lib/NetworkManager"
+    "/var/lib/iwd"
   ];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp2s0f0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp5s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
+  environment.systemPackages = with pkgs; [
+    # TUI for iwd
+    impala
+  ];
 
-  networking.networkmanager = {
-    enable = true;
-    # wifi.backend = "iwd";
+  networking = {
+    wireless.iwd = {
+      enable = true;
+      settings = {
+        # Enable built-in DHCP client
+        General.EnableNetworkConfiguration = true;
+
+        # Automatically connect to known networks
+        Settings.AutoConnect = true;
+      };
+    };
+
+    nameservers = ["1.1.1.1" "1.0.0.1" "2606:4700:4700::1111" "2606:4700:4700::1001"];
+
+    networkmanager.enable = false;
   };
 
-  users.users.${config.username}.extraGroups = ["networkmanager"];
+  users.users.${config.username}.extraGroups = ["network"];
 }
